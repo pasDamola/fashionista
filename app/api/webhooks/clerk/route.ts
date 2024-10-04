4319a2ffee0675fa70e2289f0242f26a54ea5fea
oyinda <oyincode@Featuremind-Laptop.local>
2024-10-04 13:29:29 +0100

    implement clerk webhooks

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,5 +1,6 @@
 /* eslint-disable camelcase */
-import { WebhookEvent, clerkClient } from "@clerk/nextjs/server";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { clerkClient } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook } from "svix";
